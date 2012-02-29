0d4b9d5bd656b8788fc6487fcf3506d3de677d4d
Mitchell Carroll <mitch@virus-studios.com>
2012-02-29 15:33:25 +0700

    Initial Commit

---
+++ RunRun/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  RunRun
+//
+//  Created by Mitch Carroll on 2/29/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
